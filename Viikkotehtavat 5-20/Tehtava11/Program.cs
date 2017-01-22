497bca63f70153915e10f80823799a178d28dcb6
laura <laura.hovila@hotmail.com>
2017-01-22 18:19:57 +0200

    Tehtava11

---
+++ Viikkotehtavat 5-20/Tehtava11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava11
+{
+    class Program
+    {
+        //Tee kahden sisäkkäisen for-silmukan avulla ohjelma, joka tulostaa seuraavanlaisen kuvion:
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        //Käyttäjä antaa syötteenä tähtirivien lukumäärän.Yllä olevassa esimerkissä käyttäjä on syöttänyt luvun 5.
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.Write("Anna luku: ");
+            string line = Console.ReadLine();
+            bool result = Int32.TryParse(line, out numero);
+            if (result)
+            {
+                int tähti = 1;
+                for (int i = 0; i < numero; i++)
+                {
+                    for (int k = 0; k < tähti; k++)
+                    {
+                        Console.Write("*");
+                    }
+                    tähti++;
+                    Console.WriteLine();
+                }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
