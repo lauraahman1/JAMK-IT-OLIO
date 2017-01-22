b945e6522b6c24234640893b1b61f6de06688503
laura <laura.hovila@hotmail.com>
2017-01-22 15:37:22 +0200

    Tehtava7

---
+++ Viikkotehtavat 5-20/Tehtava7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava7
+{ 
+    class Program
+    {
+        // Tee ohjelma, joka näyttää onko annettu vuosi karkausvuosi. Vuosiluku kysytään käyttäjältä.
+        static void Main(string[] args)
+        {
+            Console.Write("Anna vuosiluku: ");
+            string line = Console.ReadLine();
+            int vuosi;
+            bool result = Int32.TryParse(line, out vuosi);
+            if (result)
+            {
+                if ((vuosi % 4 == 0 && vuosi % 100 != 0) || vuosi % 400 == 0)
+                {
+                    Console.WriteLine("Vuosi on karkausvuosi.");
+                }
+                else
+                {
+                    Console.WriteLine("Vuosi ei ole karkausvuosi.");
+                }
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
