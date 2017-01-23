f823b06c005a6b141463393e5b12a5b6f46eddee
laura <laura.hovila@hotmail.com>
2017-01-23 17:40:45 +0200

    Tehtava16

---
+++ Viikkotehtavat 5-20/Tehtava16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+            int number;
+            int guesses = 0;
+            Console.WriteLine("Arvaa luku 0-100 väliltä");
+            do
+            {
+                Console.Write("Arvaa luku: ");
+                string line = Console.ReadLine();
+                bool result = Int32.TryParse(line, out number);
+                if (result)
+                {
+                    if (number < randomNumber) Console.WriteLine("Luku on isompi");
+                    else if (number > randomNumber) Console.WriteLine("Luku on pienempi");
+                    guesses++;
+                }
+              
+            } while (number != randomNumber);
+            Console.WriteLine("Hienoa, arvasit luvun" + guesses + "kerralla!");
+            Console.ReadLine();
+        }
+    }
+}
