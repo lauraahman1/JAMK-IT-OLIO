8f280029c26abd6fb8660ecb238e226e6727b1e3
laura <laura.hovila@hotmail.com>
2017-01-23 18:01:21 +0200

    Tehtava18

---
+++ Viikkotehtavat 5-20/Tehtava18/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava18
+{
+    class Program
+    {
+        //Tee ohjelma, joka kysyy käyttäjältä merkkijonon (lause). Sovelluksen tulee ilmoittaa käyttäjälle oliko annettu merkkijono palidromi.
+        static void Main(string[] args)
+        {
+            Console.Write("Kirjoita lause : ");
+            string line = Console.ReadLine();
+            int length = line.Length;
+            bool palindromi = true;
+            for (int i = 0; i < length / 2; i++)
+            {
+        
+                if (line[i] != line[length - i - 1])
+                {
+                    palindromi = false;
+                    break;
+                }
+            }
+            if (palindromi) Console.WriteLine("Kyseessä on palindromi!");
+            else Console.WriteLine("Kyseessä ei ole palindromi!");
+
+            Console.ReadLine();
+        }
+    }
+}
