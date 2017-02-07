1c90e30003861e784d83c9948174f7fb2731a6d4
Laura <laura.hovila@hotmail.com>
2017-02-07 17:19:59 +0200

    Labra5

---
+++ Labra5/Labra5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+namespace JAMK.IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tehtava1
+            Lab05teht01();
+        }
+        static void Lab05teht01()
+        {
+            //Programmed and tested by Laura 7.2.2017
+            //testataan Vehicle ja Tyre luokkia
+            //Luodaan ensimmäinen ajoneuvo
+            Vehicle auto = new JAMK.IT.Vehicle();
+            auto.Brand = "BMW";
+            auto.Model = "320";
+            //ostetaan parit renkaat
+            Tyre rinkula = new Tyre();
+            rinkula.Branch = "Hankook";
+            rinkula.Size = "205/55R16";
+            // laitetaan bemariin taakse renkaat
+            auto.Addtyre(rinkula);
+            auto.Addtyre(rinkula);
+
+            // näytetään auton tiedot
+            Console.WriteLine("Autossasi {0} on seuraavat tiedot: {1}", auto.Brand, auto.ToString());
+
+        }
+    }
+}
+
---
+++ Labra5/Labra5/Tyre.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Vehicle
+    {
+        //Constans and variables
+        //Const on määritelty määrä
+        private const int maxTyres = 4;
+        private int countTyres;
+        //properties
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public Tyre[] Tyres { get; }
+        public List<Tyre> Renkaat { get; }
+        //contructors
+        public Vehicle()
+        {
+            Tyres = new Tyre[maxTyres];
+            
+        }
+
+        //methods
+        public void Addtyre(Tyre tyre)
+        {
+            if (countTyres < maxTyres)
+            {
+                Tyres[countTyres] = tyre;
+                countTyres++;
+                Console.WriteLine("Rengas {0} lisätty ajoneuvoon {1}", tyre.ToString(), this.Brand);
+            }
+            else
+            {
+                Console.WriteLine("Ei sovi enempää renkaita, sorry");
+            }
+        }
+        //metodi jolla lisätään  listaan uusi rengas
+        public void AddtyreToList(Tyre tyre)
+        {
+
+        }
+        public override string ToString()
+        {
+            string retval = "Ajoneuvo valmistaja: " + Brand + "malli: " + Model + "\nrenkaat: ";
+            foreach (Tyre tyre in Tyres)
+            {
+                if (tyre != null)
+                    {
+                    retval += "\n" + tyre.ToString();
+                }
+            }
+            return retval;
+        }
+    }
+}
+public class Tyre
+{
+    public string Branch { get; set; }
+    
+public string Size { get; set; }
+    public override string ToString()
+    {
+        // return "Valmistaja: " + Branch + "koko" + Size;
+        //ylempi tai alempi tapa
+        return String.Format("Valmistaja on {0} koko on {1}", Branch, Size);
+    }
+}
+
