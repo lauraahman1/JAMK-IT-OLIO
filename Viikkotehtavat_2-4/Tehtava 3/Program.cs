aca4c69c82bd8c80b602168b42c46d17176fa79f
laura <laura.hovila@hotmail.com>
2017-02-13 18:46:58 +0200

    Tehtava 3

--- Viikkotehtavat_2-4/Tehtava 3/Program.cs
+++ Viikkotehtavat_2-4/Tehtava 3/Program.cs
@@ -6,10 +6,85 @@ using System.Threading.Tasks;
 
 namespace Tehtava_3
 {
-    class Program
+    abstract class Nisakas
     {
-        static void Main(string[] args)
+        //properties
+        public int Ika { get; set; }
+
+        //constructors
+        public Nisakas()
         {
+            //default constructor
+        }
+        public Nisakas(int ika)
+        {
+            Ika = ika;
+        }
+        //methods
+        public abstract void Liiku();
+        public abstract void setIka();
+
+
+    }
+    class Ihminen : Nisakas //luodaan aliluokka Nisakkaasta
+    {
+        //properties
+        public const int maxIka = 100;
+        public const int minIka = 0;
+        public int countIka = 0;
+        public int Paino { get; set; }
+        public int Pituus { get; set; }
+        public string Nimi { get; set; }
+        public List<Ihminen> Vuodet { get; }
+        //constructors
+        public Ihminen()
+        {
+            //default
+        }
+        public Ihminen(int paino, int pituus, string nimi)
+        {
+            Paino = paino;
+            Pituus = pituus;
+            Nimi = nimi;
+        }
+        //methods
+
+        public override void Liiku()
+        {
+        }
+        public override void setIka()
+        {
+            Ika++;
+        }
+        public override string ToString()
+        {
+            return "Nimi: " + Nimi + " Paino: " + Paino + " Pituus: " + Pituus + " Ikä: " + Ika;
+        }
+    }
+    class Vauva : Ihminen
+    {
+        public string Vaippa { get; set; }
+
+        public override void Liiku()
+        {
+            Console.WriteLine("Konttaa");
+        }
+        public override string ToString()
+        {
+            return "Nimi: " + Nimi + " Paino: " + Paino + " Pituus: " + Pituus + " Ikä: " + Ika + " Vaippa: " + Vaippa;
+        }
+
+    }
+    class Aikuinen : Ihminen
+    {
+        public string Auto { get; set; }
+        public override void Liiku()
+        {
+            Console.WriteLine("Kävelee");
+        }
+        public override string ToString()
+        {
+            return "Nimi: " + Nimi + " Paino: " + Paino + " Pituus: " + Pituus + " Ikä: " + Ika + " Auto: " + Auto;
         }
     }
 }
